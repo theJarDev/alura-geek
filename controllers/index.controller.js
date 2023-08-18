eadb2c52fcd6759118a79c3f578d87cceb2b134c
Christopher Gonzalez <christopher53552@gmail.com>
2023-08-17 19:36:27 -0600

    redireccion de enlaces en el index

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -10,7 +10,7 @@ const createCategory = (category) => {
     const content = `
         <div class="category__name" id="category-${counter}">
             <h3 class="category__name--title">${category}</h3>
-            <a href="./screens/all_products_client.html" class="category__name--seeAll">
+            <a href="screens/all_products_client.html" class="category__name--seeAll">
                 <div class="flex">
                 <p>Ver todo</p><img src="assets/arrow.svg" alt="flecha">
                 </div>
